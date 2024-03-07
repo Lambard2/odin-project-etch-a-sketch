667ae1c4682f192dd976b7a2f09a79054cfc1139
Filip Kilichowski <88538522+Lambard2@users.noreply.github.com>
2024-03-07 15:42:21 +0100

    added clear and color change

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 let container = document.querySelector(".container")
 let input = 16;
+let color = "black"
 function generateField(input){
     container.replaceChildren()
     for(let i = 0; i < input*input; i++){
         container.appendChild(document.createElement("div"))
     }
-    
     let sizing = document.querySelectorAll(".container div")
     for(let i = 0; i < sizing.length; i++){
+        sizing[i].addEventListener("mouseover", e=>{
+            sizing[i].style.background = color
+        })
         sizing[i].style.width = `${960/input}px`
         sizing[i].style.height = `${960/input}px`
     }
 }
 
 
-function handleButton(){
+function handleSize(){
     input = prompt("Select size")
     if(input > 100){
         alert("Too many")
         return
     }
+    generateField(input)
+}
+
+function handleColor(){
+    color = prompt("Select color")
+    let sizing = document.querySelectorAll(".container div")
+    for(let i = 0; i < sizing.length; i++){
+        sizing[i].addEventListener("mouseover", e=>{
+            sizing[i].style.background = color
+        })
+    }
+}
+
+function handleClear(){
     generateField(input)
 }
